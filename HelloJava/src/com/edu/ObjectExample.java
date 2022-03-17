594a07f381cfea7eb9a485224ac112fabbf263dd
leadon <leadon>
2022-03-17 17:58:44 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/changhoLee12345/changho.git

--- HelloJava/src/com/edu/ObjectExample.java
+++ HelloJava/src/com/edu/ObjectExample.java
@@ -1,5 +1,21 @@
 package com.edu;
+
 //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
 public class ObjectExample {
+	public static void main(String[] args) {
 
+		System.out.println("구구단 출력..");
+
+		for (int j = 0; j <= 9; j++) {
+			for (int i = 2; i <= 9; i++) {
+				if (j == 0) {
+					System.out.print("  [" + i + "단]        ");
+				} else {
+//					System.out.print(i + " * " + j + " = " + (i * j) + " ");
+					System.out.printf("%2d * %2d = %2d  ", i, j, (i*j));
+				}
+			}
+			System.out.println();
+		}
+	}
 }
