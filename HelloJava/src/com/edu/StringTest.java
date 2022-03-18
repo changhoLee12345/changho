3022984acea8228b502b2ca354e58fe273037236
chaneho <changho@email.com>
2022-03-19 04:14:47 +0900

    fd

---
+++ HelloJava/src/com/edu/StringTest.java
@@ -0,0 +1,18 @@
+package com.edu;
+
+public class StringTest {
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder();
+		for (int i = 0; i < 1000000; i++) {
+			str.append(String.valueOf(i));
+//			try {
+//				Thread.sleep(1);
+//				System.out.println(str);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+		}
+		System.out.println(str);
+		System.out.println("end of prog");
+	}
+}
