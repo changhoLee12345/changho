7f546f5c3c3e47eadbdd4aa2424bf740fb7db2ac
chaneho <changho@email.com>
2022-03-18 23:29:14 +0900

    f

--- HelloJava/src/com/edu/Gang.java
+++ HelloJava/src/com/edu/Gang.java
@@ -17,16 +17,19 @@ public class Gang {
 			i++;
 		}
 		// 초기화면..
+		System.out.println("-------------");
 		for (int i = 0; i < 9; i++) {
-			System.out.printf("%3s", "?");// number9[i]);
+			System.out.printf("%4s", "|?|");// number9[i]);
 			if (i % 3 == 2) {
 				System.out.println();
 			}
 		}
+		System.out.println("-------------");
 
 		Scanner scn = new Scanner(System.in);
 		int[] selectedAry = new int[9];
 		while (true) {
+			System.out.println();
 			System.out.print("input>> (1~9) ");
 			int val = scn.nextInt();
 			// 입력했던 값들을 배열에 누적.
@@ -36,31 +39,37 @@ public class Gang {
 					break;
 				}
 			}
-			if (number9[(val - 1)] == answer) {
-				System.out.println("correct");
-				break;
-			}
-			int correct = 0;
+
+			int correct = -1;
+			System.out.println("-------------");
 			for (int i = 0; i < number9.length; i++) {
-				correct = 0;
+				correct = -1;
 				for (int j = 0; j < selectedAry.length; j++) {
-					if (number9[i] == selectedAry[j]) {
-						correct = selectedAry[j];
+					if (i + 1 == selectedAry[j]) {
+						correct = i;
 						break;
 					}
 				}
-				if (correct != 0) {
-					System.out.printf("%3d", number9[correct - 1]);// number9[i]);
+				// 입력된 값이 있으면 그 위치를 화면에 보여주기.
+				if (correct != -1) {
+					System.out.printf(" |%1d|", number9[i]);
 				} else {
-					System.out.printf("%3s", "?");// number9[i]);
+					System.out.printf("%4s", "|?|");
 				}
 
 				if (i % 3 == 2) {
 					System.out.println();
 				}
 			}
+			System.out.println("-------------");
+			// 정답을 찾았을 경우. => 종료.
+			if (number9[(val - 1)] == answer) {
+				System.out.println("correct");
+				break;
+			}
 
 		}
+		scn.close();
 		System.out.println("end of program");
 	}
 
