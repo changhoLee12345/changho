67750069d082289ae95109a9351ce422f6a67c9d
linuxie <linuxie@my.com>
2022-03-22 21:43:37 +0900

    dfd

--- HelloGUI/src/com/edu/MyFrame.java
+++ HelloGUI/src/com/edu/MyFrame.java
@@ -1,5 +1,6 @@
 package com.edu;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
@@ -8,15 +9,21 @@ import javax.swing.JFrame;
 public class MyFrame extends JFrame {
 
 	public MyFrame() {
+
 		setSize(300, 200);
+		setLocation(200, 300);
 		setTitle("MyFrame");
-		setVisible(true);
+		setLayout(new FlowLayout());
+
+		getContentPane().setBackground(Color.yellow);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		setLayout(new FlowLayout());
-		JButton button = new JButton("button");
-		add(button);
+		JButton button1 = new JButton("OK");
+		JButton button2 = new JButton("Cancel");
+		add(button1);
+		add(button2);
 
+		setVisible(true);
 	}
 
 	public static void main(String[] args) {
