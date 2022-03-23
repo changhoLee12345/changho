7132113602885f2cb205375511720a1d586f4f0e
admin <admin@YD-00>
2022-03-23 09:12:02 +0900

    f

--- P20220321/src/com/edu/api/ApiExample.java
+++ P20220321/src/com/edu/api/ApiExample.java
@@ -1,17 +1,11 @@
 package com.edu.api;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
 public class ApiExample {
 	public static void main(String[] args) {
-		byte[] bytes;
-		try {
-			bytes = "한글".getBytes("UTF-8");
-			System.out.println(new String(bytes));
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
+		
+		IDCheck idcheck = new IDCheck();
+		String result = idcheck.getGender("9503041234567");
+		result = idcheck.getGender("950304-3234567");
 
 		System.out.println("end of prog");
 	}
