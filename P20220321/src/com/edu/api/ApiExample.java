4afd5c2ebd70f0a1a6ceb9dbfdbe1b00c5ea2900
leadon <leadon>
2022-03-23 10:26:32 +0900

    ff

--- P20220321/src/com/edu/api/ApiExample.java
+++ P20220321/src/com/edu/api/ApiExample.java
@@ -2,10 +2,12 @@ package com.edu.api;
 
 public class ApiExample {
 	public static void main(String[] args) {
-		
+
 		IDCheck idcheck = new IDCheck();
 		String result = idcheck.getGender("9503041234567");
-		result = idcheck.getGender("950304-3234567");
+		System.out.println(result);
+		result = idcheck.getGender("950304-2234567");
+		System.out.println(result);
 
 		System.out.println("end of prog");
 	}
--- P20220321/src/com/edu/api/IDCheck.java
+++ P20220321/src/com/edu/api/IDCheck.java
@@ -3,7 +3,20 @@ package com.edu.api;
 public class IDCheck {
 	// 주민번호 입력 -> 950304-1234567, 000205-4567890
 	// 950304-1234567, 950304 1234567, 9503041234567
-	
+
 	// String getGender() -> 남자, 여자.
-	
+	public String getGender(String ssno) {
+
+		ssno = ssno.replace("-", "");
+		ssno = ssno.replace(" ", "");
+
+		if (ssno.charAt(6) == '1' || ssno.charAt(6) == '3') {
+			return "남자";
+		} else if (ssno.charAt(6) == '2' || ssno.charAt(6) == '4') {
+			return "여자";
+		} else {
+			return "잘못된 번호입니다";
+		}
+	}
+
 }
