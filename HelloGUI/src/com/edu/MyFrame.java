afc20cf37dfba798b04fab93a55386731a35eb85
vsc <vsc@email>
2022-03-28 18:16:19 +0900

    dfs

--- HelloGUI/src/com/edu/MyFrame.java
+++ HelloGUI/src/com/edu/MyFrame.java
@@ -1,29 +1,49 @@
 package com.edu;
 
-import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+
 	public MyFrame() {
 
-		setSize(300, 200);
-		setLocation(200, 300);
-		setTitle("MyFrame");
-		setLayout(new FlowLayout());
+		setTitle("CardLayoutTest");
+		setSize(new Dimension(300, 150));
+		Container cPane = getContentPane();
+		CardLayout layout = new CardLayout();
 
-		getContentPane().setBackground(Color.yellow);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(layout);
 
-		JButton button1 = new JButton("OK");
-		JButton button2 = new JButton("Cancel");
-		add(button1);
-		add(button2);
+		JButton b1 = new JButton("Card #1");
+		JButton b2 = new JButton("Card #2");
+		JButton b3 = new JButton("Card #3");
 
+		add(b1);
+		add(b2);
+		add(b3);
+
+		b1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				layout.next(cPane);
+			}
+		});
+		
+		b2.addActionListener(e -> layout.next(cPane));
+		
+		b3.addActionListener(e -> layout.next(cPane));
+		
 		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 	}
 
 	public static void main(String[] args) {
