d02891efd3bf2d429c0875f9332691dfbb441e63
winnie <winnie@DESKTOP-OQVQUEO>
2022-04-12 17:55:39 +0900

    ㅇ

--- HelloWorld/src/com/edu/Work2.java
+++ HelloWorld/src/com/edu/Work2.java
@@ -1,5 +1,30 @@
 package com.edu;
 
-public class Work2 {
+import java.util.Scanner;
 
+public class Work2 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+
+		while (true) {
+			int you = (int) (Math.random() * 3) + 1; // 1=>가위, 2=>바위, 3=>보
+
+			System.out.println("가위(1), 바위(2), 보(3)");
+			int me = scn.nextInt();
+			if (me == -1) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
+
+			if (((you + 1) % 3) == (me % 3)) {
+				System.out.println("You win.");
+			} else if (you == me) {
+				System.out.println("Same");
+			} else {
+				System.out.println("You lose.");
+			}
+		} // end of while
+		System.out.println("end of prog.");
+		scn.close();
+	}
 }
