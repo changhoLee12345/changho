5d4451564a9463a7d628a5a65a61282499f19a70
vsc <vsc@email>
2022-04-19 17:46:32 +0900

    dfs

---
+++ HelloJava/src/com/nest/NestedApp.java
@@ -0,0 +1,88 @@
+package com.nest;
+
+import java.util.Scanner;
+
+class Friend {
+	String name;
+	String phone;
+	int age;
+
+	public Friend(String name, String phone, int age) {
+		super();
+		this.name = name;
+		this.phone = phone;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Friend [name=" + name + ", phone=" + phone + ", age=" + age + "]";
+	}
+
+}
+
+public class NestedApp {
+
+	Friend[] friends = new Friend[10];
+	Scanner scn = new Scanner(System.in);
+
+	interface FriendService {
+		void add();
+		void modify();
+		void list();
+		void addArray(Friend friend);
+
+	}
+
+	class FriendApp implements FriendService {
+
+		@Override
+		public void add() {
+			System.out.println("name>>");
+			String name = scn.next();
+			System.out.println("phone>>");
+			String phone = scn.next();
+			System.out.println("age>>");
+			int age = scn.nextInt();
+			Friend friend = new Friend(name, phone, age);
+			addArray(friend);
+		}
+
+		@Override
+		public void modify() {
+
+		}
+
+		@Override
+		public void list() {
+			for (Friend friend : friends) {
+				if (friend != null)
+					System.out.println(friend);
+			}
+		}
+
+		@Override
+		public void addArray(Friend friend) {
+			for (int i = 0; i < friends.length; i++) {
+				if (friends[i] == null) {
+					friends[i] = friend;
+					break;
+				}
+			}
+		}
+
+	}
+
+	public void execute() {
+		FriendApp app = new FriendApp();
+		app.add();
+		app.list();
+	}
+
+//	public static void main(String[] args) {
+//
+//		FriendApp app = new FriendApp();
+//		app.add();
+//		app.list();
+//	}
+}
---
+++ HelloJava/src/com/nest/NestedExe.java
@@ -0,0 +1,8 @@
+package com.nest;
+
+public class NestedExe {
+	public static void main(String[] args) {
+		NestedApp app = new NestedApp();
+		app.execute();
+	}
+}
